16579e327e7b404444d9bb74f2afd81eb8c11c66
Inna Cherckashina <68454245+Inna-malina@users.noreply.github.com>
2024-02-29 13:58:28 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let critic = document.querySelector('#critic');
+let neutral = document.querySelector('#neutral');
+let promoter = document.querySelector('#promoter');
+let btn = document.querySelector('button');
+
+let resalt = document.querySelector('.resalt'); 
+let npsFact = document.querySelector('.nps-fact'); 
+let res_75 = document.querySelector('.resalt-75'); 
+let res_77 = document.querySelector('.resalt-77');
+let res_79 = document.querySelector('.resalt-79');
+
+
+
+btn.addEventListener('click', function (el) {
+  el.preventDefault();
+  resalt.value = Number(critic.value) + Number(neutral.value) + Number(promoter.value);
+  npsFact.value = (Number(promoter.value) - Number(critic.value)) / resalt.value;
+  
+  let prom2 = ((0.75 * Number(critic.value)) + (0.75 * Number(neutral.value)) + Number(critic.value)) / (1 - 0.75);
+  res_75.value = prom2 - Number(promoter.value); 
+
+  let prom3 = ((0.77 * Number(critic.value)) + (0.77 * Number(neutral.value)) + Number(critic.value)) / (1 - 0.77);
+  res_77.value = prom3 - Number(promoter.value); 
+
+  let prom4 = ((0.79 * Number(critic.value)) + (0.79 * Number(neutral.value)) + Number(critic.value)) / (1 - 0.79);
+  res_79.value = prom4 - Number(promoter.value); 
+})
+
+
+
