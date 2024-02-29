fbca8468695fdf5058c3abf3df91fd39ad246b13
Inna Cherkashina <atljhbyfbyyf@mail.ru>
2024-02-29 19:01:22 +0700

    modified index.css, index.html, index.js

--- index.js
+++ index.js
@@ -10,26 +10,25 @@ let res_77 = document.querySelector('.resalt-77');
 let res_79 = document.querySelector('.resalt-79');
 
 
-
 btn.addEventListener('click', function (el) {
   el.preventDefault();
   resalt.value = Number(critic.value) + Number(neutral.value) + Number(promoter.value);
-  npsFact.value = (Number(promoter.value) - Number(critic.value)) / resalt.value * 100 + "%";
+  npsFact.value = Math.ceil((Number(promoter.value) - Number(critic.value)) / resalt.value * 100) + "%";
   
   let prom2 = ((0.75 * Number(critic.value)) + (0.75 * Number(neutral.value)) + Number(critic.value)) / (1 - 0.75);
-    res_75.value = prom2 - Number(promoter.value);  
+    res_75.value = Math.ceil(prom2 - Number(promoter.value));  
   if (res_75.value <= 0) {
     res_75.value = 0;
   } 
      
   let prom3 = ((0.77 * Number(critic.value)) + (0.77 * Number(neutral.value)) + Number(critic.value)) / (1 - 0.77);
-   res_77.value = prom3 - Number(promoter.value);   
+   res_77.value = Math.ceil(prom3 - Number(promoter.value));   
   if (res_77.value <= 0) {
     res_77.value = 0;
   }
 
   let prom4 = ((0.79 * Number(critic.value)) + (0.79 * Number(neutral.value)) + Number(critic.value)) / (1 - 0.79);
-  res_79.value = prom4 - Number(promoter.value);  
+  res_79.value = Math.ceil(prom4 - Number(promoter.value));  
   if (res_79.value <= 0) {
     res_79.value = 0;
   }
